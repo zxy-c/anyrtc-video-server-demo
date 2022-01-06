83d024f4c51ee63332dcdf4e2bf5a5d1c31878be
zxy <2544682494@qqcom>
2022-01-06 17:58:22 +0800

    配置android demo完成云端录制

--- src/main/kotlin/com/zxy/demo/anyrtcvideoserverdemo/controller/CloudRecordController.kt
+++ src/main/kotlin/com/zxy/demo/anyrtcvideoserverdemo/controller/CloudRecordController.kt
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
-@RestController("cloudRecord")
-@RequestMapping
+@RestController
+@RequestMapping("cloudRecord")
 class CloudRecordController(
     private val anyRTCClient: AnyRTCClient,
     private val anyRTCProperties: AnyRTCProperties
@@ -21,16 +22,25 @@ class CloudRecordController(
 
     @PostMapping("start")
     fun startRecord(@RequestBody startRecordRequest: StartRecordRequest) {
+        val uid = UUID.randomUUID().toString()
         val aliyun = this.anyRTCProperties.oss!!.aliyun!!
-        val resourceId = anyRTCClient.acquire(startRecordRequest.cname, startRecordRequest.uid)
+        val resourceId = anyRTCClient.acquire(startRecordRequest.cname, uid)
         anyRTCClient.startRecord(
             resourceId, AnyRTCClient.RecordMode.individual, AnyRTCClient.StartRecordRequest(
-                AnyRTCClient.StorageConfig(
-                    AnyRTCClient.StorageVendor.aliyun,
-                    AnyRTCClient.AliyunRegion.AP_Southeast_3,
-                    aliyun.bucket,
-                    aliyun.accessKeyId,
-                    aliyun.accessKeySecret
+                startRecordRequest.cname, uid,
+                AnyRTCClient.StartRecordClientRequest(
+                    AnyRTCClient.StorageConfig(
+                        AnyRTCClient.StorageVendor.aliyun,
+                        AnyRTCClient.AliyunRegion.AP_Southeast_3,
+                        aliyun.bucket,
+                        aliyun.accessKeyId,
+                        aliyun.accessKeySecret
+                    ),
+                    AnyRTCClient.RecordingConfig(
+                        subscribeAudioUids = arrayListOf(startRecordRequest.uid),
+                        subscribeVideoUids = arrayListOf(startRecordRequest.uid),
+                        subscribeUidGroup = 0
+                    )
                 )
             )
         )
--- src/main/kotlin/com/zxy/demo/anyrtcvideoserverdemo/utils/anyrtc/AnyRTCClient.kt
+++ src/main/kotlin/com/zxy/demo/anyrtcvideoserverdemo/utils/anyrtc/AnyRTCClient.kt
@@ -1,19 +1,20 @@
 package com.zxy.demo.anyrtcvideoserverdemo.utils.anyrtc
 
 import com.fasterxml.jackson.annotation.JsonValue
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpRequest
+import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.postForEntity
-import org.springframework.web.util.UriComponentsBuilder
-import java.net.URI
+import org.springframework.web.client.exchange
+import org.springframework.web.util.DefaultUriBuilderFactory
 import java.util.*
 
 class AnyRTCClient(private val customerId: String, private val customerSecret: String, private val appId: String) {
 
     private val restTemplate = RestTemplate().apply {
+        this.uriTemplateHandler = DefaultUriBuilderFactory("https://api.agrtc.cn/v1/apps/${appId}/")
         val authorizationValue =
             "Basic " + Base64.getEncoder().encodeToString("$customerId:$customerSecret".toByteArray())
         this.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
@@ -24,77 +25,114 @@ class AnyRTCClient(private val customerId: String, private val customerSecret: S
             execution.execute(request, body)
         })
         this.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            val newRequest = ReplaceUrlHttpRequest(request, appId)
-            execution.execute(newRequest, body)
+//            request.headers.contentType = MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8)
+            // 竟然不接受大写UTF-8
+            request.headers.set(HttpHeaders.CONTENT_TYPE, "application/json;charset=utf-8")
+            execution.execute(request, body)
         })
     }
 
-    private class ReplaceUrlHttpRequest(val httpRequest: HttpRequest, private val appId: String) :
-        HttpRequest by httpRequest {
-        override fun getURI(): URI {
-            return UriComponentsBuilder.fromUri(httpRequest.uri).host("https://api.agrtc.cn")
-                .replacePath("v1/apps/${appId}/" + httpRequest.uri.path.removePrefix("/"))
-                .build()
-                .toUri()
-        }
-    }
 
-    fun acquire(cname:String,uid:String): String {
-        return restTemplate.postForEntity<AnyRTCBasicResponse<AnyRTCAcquireResponse>>(
+    fun acquire(cname: String, uid: String): String {
+        val httpEntity = HttpEntity<Any>(mapOf("cname" to cname, "uid" to uid))
+        return restTemplate.exchange<AnyRTCBasicResponse<AnyRTCAcquireResponse>>(
             "cloud_recording/acquire",
-            mapOf("cname" to cname, "uid" to uid)
+            HttpMethod.POST,
+            httpEntity
         ).body!!.Body.resourceId
     }
 
     data class AnyRTCAcquireResponse(
-        val resourceId:String
+        val resourceId: String
     )
 
     @Suppress("EnumEntryName")
-    enum class RecordMode{
-        individual,mix
+    enum class RecordMode {
+        individual, mix
     }
 
     @Suppress("EnumEntryName")
     enum class StorageVendor(
         @field:JsonValue
-        val id:Int
-    ){
+        val id: Int
+    ) {
         aliyun(2)
     }
 
     interface StorageRegion {
-        val number:Int
+        val number: Int
+
         @JsonValue
         fun number(): Int {
             return this.number
         }
     }
 
-    enum class AliyunRegion(override val number: Int):StorageRegion{
+    enum class AliyunRegion(override val number: Int) : StorageRegion {
         AP_Southeast_3(12)
     }
 
     data class StorageConfig(
-        val vendor:StorageVendor,
-        val region:AliyunRegion,
-        val bucket:String,
-        val accessKey:String,
-        val secretKey:String,
-        val fileNamePrefix:List<String> = emptyList()
+        val vendor: StorageVendor,
+        val region: AliyunRegion,
+        val bucket: String,
+        val accessKey: String,
+        val secretKey: String,
+        val fileNamePrefix: List<String> = emptyList()
     )
 
     data class StartRecordRequest(
-        val storageConfig: StorageConfig? = null
+        val cname:String,
+        val uid: String,
+        val clientRequest: StartRecordClientRequest
     )
 
-    fun startRecord(resourceId: String,mode:RecordMode,startRecordRequest: StartRecordRequest = StartRecordRequest()): ResponseEntity<StartRecordResponse> {
-        return this.restTemplate.postForEntity("cloud_recording/resourceid/${resourceId}/mode/${mode.name}/start",startRecordRequest)
+    data class StartRecordClientRequest(
+        val storageConfig: StorageConfig? = null,
+        val recordingConfig:RecordingConfig= RecordingConfig()
+    )
+
+    enum class ChannelType(
+        @field:JsonValue
+        val number:Int
+    ) {
+        COMMUNICATE(0),
+        LIVE(1)
+    }
+
+    enum class StreamType(
+        @field:JsonValue
+        val number:Int
+    ){
+        AUDIO(0),
+        VIDEO(1),
+        ALL(2)
+    }
+
+
+    data class RecordingConfig(
+        val channelType:ChannelType = ChannelType.COMMUNICATE,
+        val streamTypes:StreamType = StreamType.ALL,
+        val subscribeVideoUids:List<String> = emptyList(),
+        val subscribeAudioUids:List<String> = emptyList(),
+        val subscribeUidGroup: Int? = null
+    )
+
+    fun startRecord(
+        resourceId: String,
+        mode: RecordMode,
+        startRecordRequest: StartRecordRequest
+    ): ResponseEntity<AnyRTCBasicResponse<StartRecordResponse>> {
+        return this.restTemplate.exchange(
+            "cloud_recording/resourceid/${resourceId}/mode/${mode.name}/start",
+            HttpMethod.POST,
+            HttpEntity(startRecordRequest)
+        )
     }
 
     data class StartRecordResponse(
-        val sid:String,
-        val resourceId:String
+        val sid: String,
+        val resourceId: String
     )
 
 }
