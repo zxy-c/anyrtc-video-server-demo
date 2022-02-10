bb791b1d7314b42b2da89c0dd6836866510b6af6
zxy <2544682494@qqcom>
2022-02-10 15:21:35 +0800

    Add api to generate agora rtc token

--- src/main/java/com/zxy/demo/anyrtcvideoserverdemo/controller/AgoraTempTokenController.java
+++ src/main/java/com/zxy/demo/anyrtcvideoserverdemo/controller/AgoraTempTokenController.java
@@ -1,7 +1,6 @@
 package com.zxy.demo.anyrtcvideoserverdemo.controller;
 
 import com.zxy.demo.anyrtcvideoserverdemo.service.AgoraTempTokenService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +20,11 @@ public class AgoraTempTokenController {
     @GetMapping
     public String getAgoraTempToken(@RequestParam String uid) throws Exception {
         return agoraTempTokenService.getAgoraTempToken(uid);
+    }
+    
+    @GetMapping("rtc")
+    public String getAgoraRtcToken(@RequestParam String channelName) throws Exception {
+        return agoraTempTokenService.getAgoraRtcToken(channelName);
     }
 
 }
--- src/main/java/com/zxy/demo/anyrtcvideoserverdemo/service/AgoraTempTokenService.java
+++ src/main/java/com/zxy/demo/anyrtcvideoserverdemo/service/AgoraTempTokenService.java
@@ -1,8 +1,8 @@
 package com.zxy.demo.anyrtcvideoserverdemo.service;
 
 import com.zxy.demo.anyrtcvideoserverdemo.configuration.AgoraProperties;
+import io.agora.media.AccessToken;
 import io.agora.rtm.RtmTokenBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -19,5 +19,22 @@ public class AgoraTempTokenService {
     public String getAgoraTempToken(String uid) throws Exception {
         return new RtmTokenBuilder().buildToken(agoraProperties.getAppId(),agoraProperties.getAppCertificate(),
                 uid, RtmTokenBuilder.Role.Rtm_User, Math.toIntExact(Duration.ofDays(1).toMillis()));
+    }
+
+    public String getAgoraRtcToken(String channelName) throws Exception {
+        AccessToken token = new AccessToken(
+                agoraProperties.getAppId(), agoraProperties.getAppCertificate(),
+                channelName,
+                "" // don't need to validate any user
+        );
+        int privilegeTs = (int)(System.currentTimeMillis() / 1000 + (60*60*24));
+        token.addPrivilege(AccessToken.Privileges.kJoinChannel, privilegeTs);
+        token.addPrivilege(AccessToken.Privileges.kPublishAudioStream, privilegeTs);
+        token.addPrivilege(AccessToken.Privileges.kPublishVideoStream, privilegeTs);
+        token.addPrivilege(AccessToken.Privileges.kPublishDataStream, privilegeTs);
+
+
+        return token.build();
+
     }
 }
